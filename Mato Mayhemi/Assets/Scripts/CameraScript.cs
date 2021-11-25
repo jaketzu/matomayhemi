3cc91e79a202ae90504976b5e242d8044ad983bc
Topias Illikainen <81112399+IlliTopi@users.noreply.github.com>
2021-11-25 13:50:55 +0200

    25.11 v_2
    Kamera Movement alotettu

--- Mato Mayhemi/Assets/Scripts/CameraScript.cs
+++ Mato Mayhemi/Assets/Scripts/CameraScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] objektit;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 sum = new Vector3(0, 0, 0);
+        for (int i = 0; i < objektit.Length; i++)
+        {
+            sum += objektit[i].transform.position;
+        }
+        print(sum/objektit.Length);
+
+        transform.position(sum/objektit.Length);
+        float dist = Vector3.Distance(objektit[0].transform.position, objektit[1].transform.position);
+        print(dist);
     }
 }
