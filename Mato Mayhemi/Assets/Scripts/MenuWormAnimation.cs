a71ab60942949e23ad361d8e0c120d4aabcfef03
Topias Illikainen <topias.illikainen@gmail.com>
2021-12-09 16:44:03 +0200

    Menusta tehty ihan vitun mehevä

---
+++ Mato Mayhemi/Assets/Scripts/MenuWormAnimation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuWormAnimation : MonoBehaviour
+{
+    public GameObject worm;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
