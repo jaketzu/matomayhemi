f2c566ac7b7c1ac0f3294221cabd46c59645c76f
jaketzu <jaakko.nevala007@gmail.com>
2021-12-09 14:37:18 +0200

    posahtelut ja toimiva mp
    sekä gamemanager

--- Mato Mayhemi/Assets/Scripts/MissileScript.cs
+++ Mato Mayhemi/Assets/Scripts/MissileScript.cs
@@ -7,6 +7,7 @@ public class MissileScript : MonoBehaviour
     public int damage;
     public int force;
     public float radius;
+    public LayerMask layerMask;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -15,22 +16,24 @@ public class MissileScript : MonoBehaviour
             collision.gameObject.GetComponent<Player>().TakeDamage(damage);
         }
 
-        //boom
-        RaycastHit2D[] hit = Physics2D.CircleCastAll(transform.position, radius, Vector2.zero);
-        for (int i = 0; i < hit.Length; i++)
-        {
-            print(hit[i].collider.name);
-            if(hit[i].collider.CompareTag("Player"))
+        RaycastHit2D[] hit = Physics2D.CircleCastAll(transform.position, radius, transform.right, 0, layerMask);
+            if(hit != null)
             {
-                Vector2 direction = new Vector2(hit[i].transform.position.x - transform.position.x, hit[i].transform.position.y - transform.position.y);
-                hit[i].collider.GetComponent<Player>().TakeDamage(damage);
-                hit[i].collider.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+                for (int i = 0; i < hit.Length; i++)
+                {
+                    if(hit[i].collider.CompareTag("Player"))
+                    {
+                        Vector2 direction = new Vector2(hit[i].transform.position.x - transform.position.x, hit[i].transform.position.y - transform.position.y);
+                        hit[i].collider.GetComponent<Player>().TakeDamage(damage);
+                        hit[i].collider.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+                    }
+                    else if(hit[i].collider.CompareTag("Ground"))
+                    {
+                        gameObject.GetComponent<Digging>().DestroyEnvironment();
+                    }
+                }
             }
-            else if(hit[i].collider.CompareTag("Ground"))
-            {
-                //tuhoa ympäristöä
-            }
-        }
-        Destroy(gameObject);
+            
+            Destroy(gameObject);
     }
 }
